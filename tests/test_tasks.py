3e84ec6cb578fadb43478adc9b05e7cff912749d
a.rebrov <a.rebrov@cian.ru>
2023-04-21 01:19:22 +0300

    feat: тесты

---
+++ tests/test_tasks.py
@@ -0,0 +1,61 @@
+from multiprocessing.queues import Queue
+
+import pytest as pytest
+
+from api_client import YandexWeatherAPI
+from tasks import DataFetchingTask, DataCalculationTask
+
+
+class TestDataFetchingTask:
+    def test_get_city_forecasts(self, mocker):
+        # arrange
+        cities = {'foo': 'bar'}
+        get_forecasting_mock = mocker.patch.object(
+            YandexWeatherAPI,
+            'get_forecasting',
+            return_value={'forecast': [{'item': 1}]},
+        )
+        expected_result = ('foo', {'forecast': [{'item': 1}]})
+
+        # act
+        result = DataFetchingTask(fetch_data_queue=Queue, cities=cities).get_city_forecasts('foo')
+
+        # assert
+        assert result == expected_result
+        get_forecasting_mock.assert_called_once_with(city_name='foo')
+
+    def test_get_city_forecasts__fetch_failed__raise(self, mocker, caplog):
+        # arrange
+        cities = {'foo': 'bar'}
+        get_forecasting_mock = mocker.patch.object(
+            YandexWeatherAPI,
+            'get_forecasting',
+            side_effect=Exception,
+        )
+
+        # act & assert
+        with pytest.raises(Exception):
+            DataFetchingTask(fetch_data_queue=Queue, cities=cities).get_city_forecasts('foo')
+
+        # assert
+        get_forecasting_mock.assert_called_once_with(city_name='foo')
+        assert 'Failed to fetch data from api' in caplog.text
+
+
+@pytest.mark.parametrize(
+    'hour, temp, expected_result',
+    [
+        (12, 10, 10),
+        (1, 10, None),
+    ]
+)
+class TestDataCalculationTask:
+    def test_get_daily_avg_temp(self, hour, temp, expected_result):
+        # arrange & act
+        result = (
+            DataCalculationTask(fetch_data_queue=Queue, aggregate_data_queue=Queue)
+            .get_daily_avg_temp(daily_forecast={'hours': [{'hour': hour, 'temp': temp}]})
+        )
+
+        # assert
+        assert result == expected_result
