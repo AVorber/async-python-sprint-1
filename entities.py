77657592daab126b710b4f82438c02ca50b9b382
a.rebrov <a.rebrov@cian.ru>
2023-04-21 10:09:10 +0300

    fix: исправляет докстринги

--- entities.py
+++ entities.py
@@ -4,25 +4,18 @@ from typing import Any
 
 @dataclass
 class InitialForecast:
-    city: str
-    """Название города"""
-    forecasts: list[dict[str, Any]]
-    """Прогноз погоды в городе"""
+    city: str  # Название города
+    forecasts: list[dict[str, Any]]  # Прогноз погоды в городе
 
 
 @dataclass
 class DailyTemp:
-    date: str
-    """Дата сбора прогноза погоды"""
-    avg_temp: float | None
-    """Средняя дневная температура"""
-    total_dry_hours: int
-    """Количество часов в день без осадков"""
+    date: str  # Дата сбора прогноза погоды
+    avg_temp: float | None  # Средняя дневная температура
+    total_dry_hours: int  # Количество часов в день без осадков
 
 
 @dataclass
 class CityTemp:
-    city: str
-    """Название города"""
-    daily_avg_temps: list[DailyTemp]
-    """Средняя дневная температура по дням"""
+    city: str  # Название города
+    daily_avg_temps: list[DailyTemp]  # Средняя дневная температура по дням
